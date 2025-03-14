a5e8b52ea8aa3e66602ca0b20a33d9906f35f5dd
Banti-Lodhi <166380570+Banti-Lodhi@users.noreply.github.com>
2025-03-14 23:17:15 +0530

    Update GitSystemApplication.java

--- src/main/java/com/GitSystem/GitSystemApplication.java
+++ src/main/java/com/GitSystem/GitSystemApplication.java
@@ -7,6 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GitSystemApplication {
 
 	public static void main(String[] args) {
+		int x=10;
+		int float = 70.45;
+		int long = 3534;
+		int double = 234.34;
 		SpringApplication.run(GitSystemApplication.class, args);
 	}
 
